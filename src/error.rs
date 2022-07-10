8b53e7cf81b09d272c0a6b0e5f42b6701a48a098
degaart <degaart@users.noreply.github.com>
2022-07-10 13:26:52 +0300

    Improved error handling

--- src/error.rs
+++ src/error.rs
@@ -1,50 +1,68 @@
-use crate::util::return_name;
+use std::fmt::Display;
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub enum Type {
+    Cs,
+    Client,
+    Server,
+    Library
+}
 
 #[derive(Debug, Clone)]
 pub struct Error {
+    type_: Type,
     code: Option<i32>,
-    fn_name: Option<String>,
     desc: String,
 }
 
 impl Error {
-    pub fn new(code: i32, fn_name: impl AsRef<str>) -> Self {
+    pub fn new(type_: Option<Type>, code: Option<i32>, desc: impl AsRef<str>) -> Self {
         Self {
-            code: Some(code),
-            fn_name: Some(fn_name.as_ref().to_string()),
-            desc: format!("{} failed (ret: {})", fn_name.as_ref(), return_name(code).unwrap_or(&format!("{}", code)))
+            type_: match type_ {
+                None => Type::Library,
+                Some(type_) => type_
+            },
+            code: code,
+            desc: desc.as_ref().to_string()
         }
     }
 
     pub fn from_message(desc: impl AsRef<str>) -> Self {
-        Self {
-            code: None,
-            fn_name: None,
-            desc: desc.as_ref().into()
-        }
+        Self::new(None, None, desc)
+    }
+
+    pub fn from_failure(fn_name: impl AsRef<str>) -> Self {
+        Self::new(None, None, &format!("{} failed", fn_name.as_ref()))
     }
 
     pub fn code(&self) -> Option<i32> {
         self.code
     }
 
-    pub fn fn_name(&self) -> Option<&str> {
-        if let Some(s) = &self.fn_name {
-            Some(s)
-        } else {
-            None
-        }
-    }
-
     pub fn desc(&self) -> &str {
         &self.desc
+    }
+
+    pub fn type_(&self) -> Type {
+        self.type_
     }
 
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.desc)
+        match self.type_ {
+            Type::Cs => { write!(f, "CS library error")?; },
+            Type::Client => { write!(f, "Client library error")?; },
+            Type::Server => { write!(f, "Server error")?; },
+            Type::Library => { write!(f, "FreeTDS error")?; }
+        };
+
+        if let Some(code) = self.code {
+            write!(f, " #{:04}", code)?;
+        }
+
+        write!(f, ": {}", self.desc)
     }
 }
 
@@ -63,18 +81,3 @@ impl From<std::ffi::NulError> for Error {
         Self::from_message(e.to_string())
     }
 }
-
-macro_rules! err {
-    ($code:ident, $fn_name:ident) => {
-        Err(Error::new($code, stringify!($fn_name)))
-    };
-    ($desc:literal) => {
-        Err(Error::from_message($desc))
-    };
-    ($desc:literal, $($arg:tt)*) => {
-        Err(Error::from_message(format!($desc, $($arg)*)))
-    };
-}
-use std::fmt::Display;
-
-pub(crate) use err;
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use freetds_sys::*;
 pub mod connection;
 pub mod command;
 pub mod property;
@@ -12,11 +11,6 @@ pub use error::Error;
 pub use null::NULL;
 
 pub type Result<T, E = error::Error> = core::result::Result<T, E>;
-
-extern "C" {
-    #[allow(dead_code)]
-    fn debug1(ctx: *mut CS_CONTEXT) -> i32;
-}
 
 #[cfg(test)]
 mod tests {
