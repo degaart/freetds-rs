7fada8549ff3e02d68f29dd34c1650d0c64fdf22
degaart <degaart@users.noreply.github.com>
2023-02-04 21:56:03 +0300

    Implement Display for Error::Type

--- src/error.rs
+++ src/error.rs
@@ -8,6 +8,12 @@ pub enum Type {
     Library,
 }
 
+impl Display for Type {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
 impl Default for Type {
     fn default() -> Self {
         Self::Library
